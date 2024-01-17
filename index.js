fcab728fa3d5d7ce7eb8f6e5278f62f90ecf748f
Roland Levy <contact@rolandlevy.co.uk>
2024-01-17 11:26:19 +0000

    fixed formatting issues

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ require('dotenv').config();
 const app = require('./app.js');
 const { PORT = 3000, MONGODB_URI } = process.env;
 
-mongoose.connect(MONGODB_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-})
-.then(client => {
-  console.log("Database Connected Successfully");
-  app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`);
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then((client) => {
+    console.log('Database Connected Successfully');
+    app.listen(PORT, () => {
+      console.log(`Server started at port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
   });
-})
-.catch(err => {
-  console.error(err.stack);
-  process.exit(1);
-});
